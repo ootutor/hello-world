9ce4b11f5dae2289702cd26dc32a37c6d250c30c
alantam-polyu <alantam.polyu@gmail.com>
2015-03-26 20:43:02 +0800

    my first commit

--- hello-world/src/firstclass.java
+++ hello-world/src/firstclass.java
@@ -1,3 +1,5 @@
+import java.util.Calendar;
+
 public class firstclass {
 	public static void main(String[] argv)
 	{
@@ -7,6 +9,9 @@ public class firstclass {
 
 		
 		System.out.println("I am Stefan Lazar");
+		
+		System.out.println("my changes here");
+		
 		
 		// Hello, here is a fake conflict.
 	

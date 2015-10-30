072faf2cd69adbf38954501ef08319cf9d8e8ae0
ootutor <git2014oo@gmail.com>
2015-10-30 18:28:14 +0800

    update for oo lab 2015 autumn

--- hello-world/src/firstclass.java
+++ hello-world/src/firstclass.java
@@ -2,86 +2,8 @@ import java.util.Calendar;
 // hello world
 public class firstclass {
 	public static void main(String[] argv)
-	{	
-		//1234567
-		//added by ssssssssssss111111111111
-		//System.out.println("above all");
+	{
 		System.out.println("Hello!!!!");
-
-	    System.out.println("Good Bye");
-
-
-		System.out.println("I am Stefan Lazar");
-		System.out.println("I am who i am");
-
-		System.out.println("_(:3 ");
-
-		System.out.println("OO lab testing");
-
-
-		System.out.println("testing for OO lab, haha");
-
-		System.out.println("my changes here");
-		
-		System.out.println("my second changes here");
-		
-
-
-		System.out.println("My one and Only changes here");
-
-		System.out.println("My more and more and more changes here");
-		System.out.println("Dave Test");
-		
-		// Hello, here is a fake conflict.
-
-	
-
-	
-		System.out.println("testing123");
-
-
-		System.out.println("testing 1234");
-
- 
-
-		
-		System.out.println("first class");
-		
-		System.out.println("Testing, ppppppppp");
-		System.out.println("Testing, dfsdfds");
-		System.out.println("Testing, dfsdfds");
-		System.out.println("Testing, ppppppppp");
-		System.out.println("Testing, dfsdfds");
-		System.out.println("Testing, dfsdfds");
-
-		System.out.println("wwwwwww");
-		
-	//aaaaa
-		
-
-		
-		System.out.println("Testing");
-	
-/// 112
-
-		System.out.println("Testing, dfsdfds");
-		System.out.println("Testing, dfsdfds");
-		System.out.println("Testing, ppppppppp");
-		System.out.println("Testing, dfsdfds");
-		System.out.println("Testing, dfsdfds");
-
- //branch 'master' of https://github.com/ootutor/hello-world
-	//aaaaa
-	
-	//kk
-		System.out.println("ABCDE");
-
-	//abc
-		
-		System.out.println(" 123456");
-
-	//abc	
-
  
 	}
 }

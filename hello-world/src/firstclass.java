45710038896b507dbb8af28852ed9371d109012e
holystar1076 <holystar1076@live.hk>
2015-11-02 10:56:57 +0800

    Only hello no world by LeoTsui

--- hello-world/src/firstclass.java
+++ hello-world/src/firstclass.java
@@ -1,11 +1,8 @@
 public class firstclass {
 	public static void main(String[] argv)
 	{
-		System.out.println("Oh");
-		System.out.println("Good morning world");
-
-		System.out.println("hello world");
-		System.out.println("hello world !");
+		System.out.println("Oh hello!");
+		System.out.println("OKIDOKI");
 	}
 }
 

b1d7a10a7c99c1b59341ba2b01ddb88cc80c603a
kowf <kathy.kowg@gmail.com>
2015-11-02 14:09:34 +0800

    add a line

--- hello-world/src/firstclass.java
+++ hello-world/src/firstclass.java
@@ -2,6 +2,7 @@ public class firstclass
 {
 	public static void main(String[] argv)
 	{
+		System.out.println("Trying hard to speak and driving to the deep end");
 		System.out.println("Hey, everyone!!! Are you still sleeping!?");
 		System.out.println("Don't push me again! I almost fall into the sea!");
 	}

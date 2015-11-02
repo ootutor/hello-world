cb8c17c2ae109cb5ae7d118c12a619a242e71aec
raymondchan2120 <chanwingshing19960223@gmail.com>
2015-11-02 13:58:50 +0800

    Output a message.

--- hello-world/src/firstclass.java
+++ hello-world/src/firstclass.java
@@ -2,7 +2,7 @@ public class firstclass
 {
 	public static void main(String[] argv)
 	{
-		System.out.println("Hello, world!");
+		System.out.println("Hey, everyone!!! Are you still sleeping!?");
+		System.out.println("Don't push me again! I almost fall into the sea!");
 	}
 }
-

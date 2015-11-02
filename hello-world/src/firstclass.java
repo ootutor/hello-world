57178b65fda653173ffdadd081e1a74730b5ae58
ootutor <git2014oo@gmail.com>
2015-11-02 11:21:15 +0800

    for lab updated

--- hello-world/src/firstclass.java
+++ hello-world/src/firstclass.java
@@ -1,13 +1,6 @@
 public class firstclass {
 	public static void main(String[] argv)
 	{
-		System.out.println("Oh");
-		System.out.println("Good morning world");
-//1234
-		System.out.println("hello world");
-		System.out.println("hello world !");
-		System.out.println("Raphael is god!");
-		
 	}
 }
 
